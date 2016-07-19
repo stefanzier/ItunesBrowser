eb82110ffab3e04878c9e811c04482ac3e03f793
Stefan Zier <szier@icloud.com>
2016-07-18 20:16:52 -0700

    Configured Basic Styles

--- index.ios.js
+++ index.ios.js
@@ -7,47 +7,33 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
+  View,
   Text,
-  View
+  StyleSheet,
+  StatusBar  
 } from 'react-native';
+
+import styles from './styles';
 
 class ItunesBrowser extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View style={styles.mainContainer}>
+        <StatusBar barStyle="light-content" />
+        <View style={styles.appearance}>
+          <Text style={styles.button}></Text>
+          <Text style={styles.title}>ItunesBrowser</Text>
+          <Text style={styles.button}>Search</Text>
+        </View>        
+
+        <View style={styles.content}>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, sint ipsa? Soluta culpa ea velit recusandae quos dolores iste assumenda ipsum cupiditate sunt neque ducimus, nisi fugit cumque dignissimos nam.
+          </Text>
+        </View>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('ItunesBrowser', () => ItunesBrowser);
---
+++ styles.js
@@ -0,0 +1,32 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  mainContainer: {
+    flex: 1
+  },
+  
+  content: {
+    flex: 1,
+    backgroundColor: '#CCC',
+  },
+  
+  appearance: {
+    backgroundColor: '#2A3744',
+    paddingTop: 30,
+    paddingBottom: 10,
+    flexDirection: 'row'
+  },
+
+  title: {
+    color: '#FEFEFE',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    flex: 1
+  },
+
+  button: {
+    width: 50,
+    color: '#FEFEFE',
+    textAlign: 'center'
+  }  
+});
